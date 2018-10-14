e007e80ee0a933db578155a397367f775c8c2f9e
DaveLangcaster <dlangcaster@gmail.com>
2018-10-14 14:13:59 +0100

    Upload plot3.png and plot3.R

---
+++ plot3.R
@@ -0,0 +1,25 @@
+## Question 3 
+
+# Of the four types of sources indicated by the type (point, nonpoint, onroad, nonroad) variable, which of these four sources have seen decreases in emissions from 1999–2008 for Baltimore City? Which have seen increases in emissions from 1999–2008? Use the ggplot2 plotting system to make a plot answer this question.
+
+## Load the required library
+
+library(ggplot2)
+
+## Subset and aggregate the data
+
+subNEI  <- NEI[NEI$fips=="24510", ]
+
+allYearsAndTypesBaltimore <- aggregate(Emissions ~ year + type, subNEI, sum)
+
+png("plot3.png", width=640, height=480)
+
+g <- ggplot(allYearsAndTypesBaltimore, aes(year, Emissions, color = type))
+g <- g + geom_line() + xlab("year") + ylab(expression('Total PM'[2.5]*" Emissions")) + ggtitle('Total Emissions in Baltimore City, Maryland (fips == "24510") from 1999 to 2008')
+
+
+print(g)
+dev.off()
+
+## The answer is unclear for the 'point' data - emissions increased from 1999 to 2005, but then rapidly dropped between 2005 and 2008 to what looks like a similar level to 1999. All other emissions have dropped.
+
