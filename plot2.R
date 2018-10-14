9e87611b81895425203056a893682818ffe716ca
DaveLangcaster <dlangcaster@gmail.com>
2018-10-14 14:31:04 +0100

    Upload plot4.png and plot4.R. Slight changes to plot2.R

--- plot2.R
+++ plot2.R
@@ -2,12 +2,18 @@
 
 # Have total emissions from PM2.5 decreased in the Baltimore City, Maryland from 1999 to 2008? Use the base plotting system to make a plot answering this question.
 
+## Subset and aggregate the data
+
 subNEI  <- NEI[NEI$fips=="24510", ]
 
 allYearsBaltimore <- aggregate(Emissions ~ year, subNEI, sum)
 
+## Plot the data
+
 png('plot2.png')
+
 barplot(height=allYearsBaltimore$Emissions, names.arg=allYearsBaltimore$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
+
 dev.off()
 
 ## The answer is yes, in general the emissions have decreased, but there was a spike in 2005.
---
+++ plot4.R
@@ -0,0 +1,29 @@
+## Question 4
+
+# Across the United States, how have emissions from coal combustion-related sources changed from 1999–2008?
+
+## Load the required library
+
+library(ggplot2)
+
+## Merge the two data sets
+
+NEISCC <- merge(NEI, SCC, by="SCC")
+
+## Subset the data for records with Short.Name = "coal"
+
+coalRecords  <- grepl("coal", NEISCC$Short.Name, ignore.case=TRUE)
+subNEISCC <- NEISCC[coalRecords, ]
+
+## Aggregate the data
+
+allYearsCoal <- aggregate(Emissions ~ year, subNEISCC, sum)
+
+## Plot the data
+
+png("plot4.png", width=640, height=480)
+g <- ggplot(allYearsCoal, aes(factor(year), Emissions))
+g <- g + geom_bar(stat="identity") + xlab("year") + ylab(expression('Total PM'[2.5]*" Emissions")) + ggtitle('Total Emissions from coal sources from 1999 to 2008')
+
+print(g)
+dev.off()
